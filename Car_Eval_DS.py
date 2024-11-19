a9a601b6012f0758c7790029f4b421b7a7049f81
adichn <adityachauhan1712004.com>
2024-11-19 18:22:39 -0500

    Update Car_Eval_DS.py

--- Car_Eval_DS.py
+++ Car_Eval_DS.py
@@ -1,14 +1,16 @@
-from ucimlrepo import fetch_ucirepo 
-  
-# fetch dataset 
-car_evaluation = fetch_ucirepo(id=19) 
-  
-# data (as pandas dataframes) 
-X = car_evaluation.data.features 
-y = car_evaluation.data.targets 
-  
-# metadata 
-print(car_evaluation.metadata) 
-  
-# variable information 
-print(car_evaluation.variables) 
+import ssl
+from ucimlrepo import fetch_ucirepo
+
+# Disable SSL verification
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Fetch the dataset
+car_evaluation = fetch_ucirepo(id=19)
+
+# Features and targets
+X = car_evaluation.data.features
+y = car_evaluation.data.targets
+
+# Print metadata
+print("Metadata:\n", car_evaluation.metadata)
+print("Variables:\n", car_evaluation.variables)
