1bbf127f53065d80354df66141fbc6f369519236
adichn <adityachauhan1712004.com>
2024-11-19 18:45:51 -0500

    Update Car_Eval_DS.py

--- Car_Eval_DS.py
+++ Car_Eval_DS.py
@@ -1,16 +1,23 @@
-import ssl
-from ucimlrepo import fetch_ucirepo
+from ucimlrepo import fetch_ucirepo, list_available_datasets
 
-# Disable SSL verification
-ssl._create_default_https_context = ssl._create_unverified_context
+list_available_datasets()
 
-# Fetch the dataset
-car_evaluation = fetch_ucirepo(id=19)
+## Possible Options
 
-# Features and targets
-X = car_evaluation.data.features
-y = car_evaluation.data.targets
 
-# Print metadata
-print("Metadata:\n", car_evaluation.metadata)
-print("Variables:\n", car_evaluation.variables)
+# Auto MPG dataset
+
+# Auto_MPG = fetch_ucirepo(id = 9)
+# print(Auto_MPG)
+
+
+# Automobile dataset
+
+# Automobile = fetch_ucirepo(id = 10)
+# print(Automobile)
+
+
+# Contraeptive Method Choice
+
+CMC = fetch_ucirepo(id = 30)
+print(CMC)
